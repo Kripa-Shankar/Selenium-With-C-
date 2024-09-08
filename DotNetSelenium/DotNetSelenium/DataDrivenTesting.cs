c4422c6e0a471d383e786e1d8e06be2315f4b1bd
Kripa Rakshit <kripa.rakshit@optym.com>
2024-09-08 14:36:04 +0530

    Data Driven Testing

---
+++ DotNetSelenium/DotNetSelenium/DataDrivenTesting.cs
@@ -0,0 +1,75 @@
+﻿using DotNetSelenium.Pages;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSelenium
+{
+    public class DataDrivenTesting
+    {
+        private IWebDriver _driver;
+      
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("http://eaapp.somee.com");
+            _driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        [Category("ddt")]
+        [TestCaseSource(nameof(Login))]
+        public void TestUsingPOM(LoginModel loginModel)
+        {
+
+            LoginPage loginPage = new LoginPage(_driver);
+
+            loginPage.ClickLogin();
+
+            loginPage.Login(loginModel.UserName, loginModel.Password);
+        }
+
+        public static IEnumerable<LoginModel> Login()
+        {
+            yield return new LoginModel()
+            {
+                UserName = "admin",
+                Password = "password"
+            };
+
+            yield return new LoginModel()
+            {
+                UserName = "admin2",
+                Password = "password2"
+            };
+
+            yield return new LoginModel()
+            {
+                UserName = "admin3",
+                Password = "password3"
+            };
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            if (_driver != null)
+            {
+                _driver.Quit();
+
+                
+                _driver.Dispose();
+            }
+        }
+
+
+
+
+
+    }
+}
